2f229393ad2282d0902ff91db3a4ecc6d9ff8ff1
GioAbuladze-Creator <giorgi.abuladze.1@btu.edu.ge>
2023-08-17 00:50:15 +0400

    tested: works fine

--- Subjects.mjs
+++ Subjects.mjs
@@ -1,40 +1,40 @@
 export default class Subjects {
     #subjectList;
     #checkValidFields(subj) {
-        let validFields = ['title', 'lessons','description'];
-        for(let i in subj){
-            if(!validFields.includes(i)){
-                throw new Error('Invalid Object property: '+i)
+        let validFields = ['title', 'lessons', 'description'];
+        for (let i in subj) {
+            if (!validFields.includes(i)) {
+                throw new Error('Invalid Object property: ' + i)
             }
         }
     }
-    #checkSubject(subj){
+    #checkSubject(subj) {
         if (typeof subj !== 'object' || Array.isArray(subj) || subj === null || Object.keys(subj) == 0) {
             throw new Error("Invalid Object")
         }
-        if(!subj.hasOwnProperty('title')||!subj.hasOwnProperty('lessons')){
+        if (!subj.hasOwnProperty('title') || !subj.hasOwnProperty('lessons')) {
             throw new Error('Invalid Object')
         }
-        if(typeof subj.title !== 'string'||typeof subj.lessons !== 'number' || isNaN(subj.lessons) || subj.lessons<0){
+        if (typeof subj.title !== 'string' || typeof subj.lessons !== 'number' || isNaN(subj.lessons) || subj.lessons < 0) {
             throw new Error('Invalid Object')
         }
-        if(subj.hasOwnProperty('description')&&typeof subj.description !== 'string'){
+        if (subj.hasOwnProperty('description') && typeof subj.description !== 'string') {
             throw new Error('Invalid Object')
-        }    
+        }
     }
     constructor() {
         this.#subjectList = new Map();
     }
     add(subj) {
-        if(arguments.length !== 1){
+        if (arguments.length !== 1) {
             throw new Error('Invalid arguments');
         }
         this.#checkSubject(subj);
         this.#checkValidFields(subj);
-        
+
         // checks subject title and lesson number and throws error if subject already exists
-        for(let i of this.#subjectList.values()){
-            if(i.title === subj.title&&i.lessons === subj.lessons&&i.description === subj.description){
+        for (let i of this.#subjectList.values()) {
+            if (i.title === subj.title && i.lessons === subj.lessons && i.description === subj.description) {
                 throw new Error('Subject already exists');
             }
         }
@@ -46,7 +46,7 @@ export default class Subjects {
         if (arguments.length !== 1) {
             throw new Error('Invalid arguments');
         }
-        if(typeof id !== 'string' || id.length === 0){
+        if (typeof id !== 'string' || id.length === 0) {
             throw new Error('Invalid id')
         }
         if (this.#subjectList.has(id)) {
@@ -61,24 +61,34 @@ export default class Subjects {
         }
         this.#checkSubject(subj);
         this.#checkValidFields(subj);
-        
-        for(let i of this.#subjectList.values()){
-            if(subj.title===i.title){
+
+        for (let i of this.#subjectList.values()) {
+            if (subj.title === i.title) {
                 return true;
             }
         }
         return false;
-        
+
     }
     readAll() {
         if (arguments.length !== 0) {
             throw new Error('Invalid arguments');
         }
         // add id in each subject
-        for(let i of this.#subjectList){
-            i[1].id=i[0];
+        for (let i of this.#subjectList) {
+            i[1].id = i[0];
         }
         return [...this.#subjectList.values()];
     }
 
 }
+let subjects = new Subjects();
+const history = {
+    title: 'History',
+    lessons: 24
+};
+let subjectId=subjects.add(history);
+subjects.verify(history);
+console.log(subjects.readAll());
+subjects.remove(subjectId);
+console.log(subjects.readAll());
